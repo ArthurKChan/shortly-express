2acab836d70e931659f60a06569d26a7592e20b2
Hack Reactor Students <accounts@hackreactor.com>
2014-11-26 14:55:54 -0800

    make urls shortening tests pass

--- app/config.js
+++ app/config.js
@@ -24,7 +24,7 @@ db.knex.schema.hasTable('urls').then(function(exists) {
       link.integer('visits');
       link.timestamps();
     }).then(function (table) {
-      console.log('Created Table', table);
+      console.log('Created URLs Table', table);
     });
   }
 });
@@ -36,7 +36,7 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
       click.integer('link_id');
       click.timestamps();
     }).then(function (table) {
-      console.log('Created Table', table);
+      console.log('Created Clicks Table', table);
     });
   }
 });
@@ -45,5 +45,16 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
 // Add additional schema definitions below
 /************************************************************/
 
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 255);
+      user.string('password', 255);
+    }).then(function (table) {
+      console.log('Created Users Table', table);
+    });
+  }
+});
 
 module.exports = db;
--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,15 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: false,
+
+  initialize: function(){
+    // this.on('creating', function(model, attrs, options){
+    //   var hash = bcrypt.hashSync(model.get('password'));
+    //   model.set('hashedPassword', hash);
+    // });
+  }
 });
 
 module.exports = User;
